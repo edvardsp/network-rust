c4eff2cbe226059f1c2ebcc94d6992409c64e432
edvardsp <edvard.pettersen@gmail.com>
2017-01-17 11:37:25 +0100

    Added custom struct pkt

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,31 @@
 
 #![feature(mpsc_select)]
 
+#[macro_use]
+extern crate serde_derive;
 extern crate rand;
-extern crate network_rust;
+extern crate chrono;
 
 use std::io;
 use std::thread;
 use std::sync::mpsc::channel;
 
 use rand::Rng;
+use chrono::offset::local::Local;
 
+extern crate network_rust;
 use network_rust::localip::get_localip;
 use network_rust::peer::{PeerTransmitter, PeerReceiver, PeerUpdate};
 use network_rust::bcast::{BcastTransmitter, BcastReceiver};
 
 const PEER_PORT: u16 = 9877;
 const BCAST_PORT: u16 = 9876;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct MyPacket {
+    msg: String,
+    timestamp: i64,
+}
 
 fn main() {
     let unique = rand::thread_rng().gen::<u16>();
@@ -35,8 +45,8 @@ fn main() {
     });
 
     // Spawn broadcast transmitter and receiver
-    let (transmit_tx, transmit_rx) = channel::<String>();
-    let (receive_tx, receive_rx) = channel::<String>();
+    let (transmit_tx, transmit_rx) = channel::<MyPacket>();
+    let (receive_tx, receive_rx) = channel::<MyPacket>();
     thread::spawn(move|| {
         BcastTransmitter::new(BCAST_PORT)
             .expect("Error creating ")
@@ -54,7 +64,10 @@ fn main() {
             let mut input = String::new();
             io::stdin().read_line(&mut input)
                 .expect("Couldn't read line");
-            transmit_tx.send(input.trim().to_string()).unwrap();
+            transmit_tx.send(MyPacket {
+                msg:       input.trim().to_string(),
+                timestamp: Local::now().timestamp(),
+            }).unwrap();
         }
     });
 
@@ -65,7 +78,7 @@ fn main() {
                 println!("{}", update.unwrap());
             },
             bcast_msg = receive_rx.recv() => {
-                println!("Got bcast_msg: {}", bcast_msg.unwrap());
+                println!("Got bcast_msg: {:?}", bcast_msg.unwrap());
             }
         }
     }
